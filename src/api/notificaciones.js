97ab8720b681afab547d9bc85f33b7c44e122750
Maxandresb <maxandresb@gmail.com>
2023-10-15 20:57:18 -0300

    update

--- src/api/notificaciones.js
+++ src/api/notificaciones.js
@@ -2,6 +2,13 @@ import * as Notifications from 'expo-notifications';
 import { useEffect } from "react";
 
 
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    }),
+  });
 export async function scheduleRecordatorioNotification(recordatorio) {
     let data = recordatorio;
     var fecha = data.Fecha.split('-');
@@ -16,6 +23,7 @@ export async function scheduleRecordatorioNotification(recordatorio) {
     console.log(`${year}  : ${month} : ${day}  : ${hour} : ${minute}`)
     await Notifications.scheduleNotificationAsync({
       content: {
+        sound:'default',
         title: data.Titulo,
         body: data.Descripcion,
         data: { data: 'aun en trabajo' },
