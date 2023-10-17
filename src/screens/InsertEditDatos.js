8572be36f4e678790bb44cd97f7180ea41385220
Jorge-M-Manriquez <jorg.montecinos@duocuc.cl>
2023-10-17 03:17:47 -0300

    insertar datos usuario con picker | no guta mxo

--- src/screens/InsertEditDatos.js
+++ src/screens/InsertEditDatos.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, Button, TextInput, View, Text, ScrollView } from 'react-native';
+import { StyleSheet, Button, TextInput, View, Text, ScrollView, TouchableOpacity } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import * as SQLite from 'expo-sqlite';
 const db = SQLite.openDatabase('adamdb.db');
 const InsertEdirDatos = () => {
-  const [rut, setRut] = useState('');  
+  const [rut, setRut] = useState('');
   const [pnombre, setPNombre] = useState('');
   const [snombre, setSNombre] = useState('');
   const [papellido, setPApellido] = useState('');
@@ -21,15 +22,27 @@ const InsertEdirDatos = () => {
       tx.executeSql(
         'INSERT INTO Usuario (rut, pnombre, snombre, papellido, sapellido, alias, genero, tipo_sangre, fecha_nacimiento, alergias, cronico, donante) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
         [rut, pnombre, snombre, papellido, sapellido, alias, genero, tipo_sangre, fecha_nacimiento, alergias, cronico, donante],
-        () =>{}, 
+        () => { },
         (_, error) => console.log('Error al insertar datos en la tabla Usuario:', error)
       );
     });
     console.log('DATOS DEL USUARIO INGRESADOS CORRECTAMENTE')
   }
+  //datepicker para fec_nac
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showDatepicker = () => {
+    setShow(true);
+  };
 
 
-  
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.header}>Ingresa tu Rut: </Text>
@@ -81,73 +94,94 @@ const InsertEdirDatos = () => {
         value={alias}
       />
       <Text style={styles.header}>Indica tu genero: </Text>
-      <Picker
-        selectedValue={genero}
-        onValueChange={(itemValue) => setGenero(itemValue)}
-        style={styles.input}
+      <View style={styles.inputPicker}>
+        <Picker
+          selectedValue={genero}
+          onValueChange={(itemValue) => setGenero(itemValue)}
+          style={styles.input}
         >
-        <Picker.Item label="Toca aqui para seleccionar una opción" value="" />
-        <Picker.Item label="Hombre" value="Hombre" />
-        <Picker.Item label="Mujer" value="Mujer" />
-        <Picker.Item label="No Binario" value="No Binario" />
-        <Picker.Item label="Prefiero no decirlo" value="Prefiero no decirlo" />
-      </Picker>
+          <Picker.Item label="Toca aqui para seleccionar una opción" value="" />
+          <Picker.Item label="Hombre" value="Hombre" />
+          <Picker.Item label="Mujer" value="Mujer" />
+          <Picker.Item label="No Binario" value="No Binario" />
+          <Picker.Item label="Prefiero no decirlo" value="Prefiero no decirlo" />
+        </Picker>
+      </View>
       <Text style={styles.header}>Selecciona tu tipo de sangre: </Text>
-      <Picker
-        selectedValue={tipo_sangre}
-        onValueChange={(itemValue) => setTipoSangre(itemValue)}
-        style={styles.input}
+      <View style={styles.inputPicker}>
+        <Picker
+          selectedValue={tipo_sangre}
+          onValueChange={(itemValue) => setTipoSangre(itemValue)}
+          style={styles.input}
         >
-        <Picker.Item label="Toca aqui para seleccionar una opción" value="" />
-        <Picker.Item label="O Positivo" value="O Positivo" />
-        <Picker.Item label="O Negativo" value="O Negativo" />
-        <Picker.Item label="A Positivo" value="A Positivo" />
-        <Picker.Item label="A Negativo" value="A Negativo" />
-        <Picker.Item label="B Positivo" value="B Positivo" />
-        <Picker.Item label="B Negativo" value="B Negativo" />
-        <Picker.Item label="AB Positivo" value="AB Positivo" />
-        <Picker.Item label="AB Negativo" value="AB Negativo" />
+          <Picker.Item label="Toca aqui para seleccionar una opción" value="" />
+          <Picker.Item label="O Positivo" value="O Positivo" />
+          <Picker.Item label="O Negativo" value="O Negativo" />
+          <Picker.Item label="A Positivo" value="A Positivo" />
+          <Picker.Item label="A Negativo" value="A Negativo" />
+          <Picker.Item label="B Positivo" value="B Positivo" />
+          <Picker.Item label="B Negativo" value="B Negativo" />
+          <Picker.Item label="AB Positivo" value="AB Positivo" />
+          <Picker.Item label="AB Negativo" value="AB Negativo" />
 
-      </Picker>
+        </Picker>
+      </View>
       <Text style={styles.header}>Selecciona tu fecha de nacimiento: </Text>
-      <TextInput
-        style={styles.input}
-        placeholderTextColor="gray"
-        placeholder="Fecha de nacimiento"
-        onChangeText={text => setFechaNacimiento(text)}
-        value={fecha_nacimiento}
-      />
+      <TouchableOpacity onPress={() => setShow(true)}>
+        <TextInput
+          style={styles.input}
+          placeholder="Fecha de nacimiento"
+          value={date.toLocaleDateString()}
+          editable={false}
+        />
+      </TouchableOpacity>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={'date'}
+          is24Hour={true}
+          display="spinner"
+          onChange={onChange}
+        />
+      )}
       <Text style={styles.header}>Indica si posees o no alergias: </Text>
-      <Picker
-        selectedValue={alergias}
-        onValueChange={(itemValue) => setAlergias(itemValue)}
-        style={styles.input}
+      <View style={styles.inputPicker}>
+        <Picker
+          selectedValue={alergias}
+          onValueChange={(itemValue) => setAlergias(itemValue)}
+          style={styles.input}
         >
-        <Picker.Item label="Toca aqui para seleccionar una opción" value="" />
-        <Picker.Item label="Sí" value="Sí" />
-        <Picker.Item label="No" value="No" />
-      </Picker>
+          <Picker.Item label="Toca aqui para seleccionar una opción" value="" />
+          <Picker.Item label="Sí" value="Sí" />
+          <Picker.Item label="No" value="No" />
+        </Picker>
+      </View>
       <Text style={styles.header}>Indica si posees o no patologias cronicas: </Text>
-      <Picker
-        selectedValue={cronico}
-        onValueChange={(itemValue) => setCronico(itemValue)}
-        style={styles.input}
+      <View style={styles.inputPicker}>
+        <Picker
+          selectedValue={cronico}
+          onValueChange={(itemValue) => setCronico(itemValue)}
+          style={styles.input}
+          itemStyle={{ textAlign: 'left', textAlignments: 'left', textAlignmentsVertical: 'center' }}
         >
-        <Picker.Item label="Toca aqui para seleccionar una opción" value="" />
-        <Picker.Item label="Sí" value="Sí" />
-        <Picker.Item label="No" value="No" />
-      </Picker>
+          <Picker.Item label="Toca aqui para seleccionar una opción" value="" />
+          <Picker.Item label="Sí" value="Sí" />
+          <Picker.Item label="No" value="No" />
+        </Picker>
+      </View>
       <Text style={styles.header}>Selecciona si eres o no donante: </Text>
-      <Picker
-        selectedValue={donante}
-        onValueChange={(itemValue) => setDonante(itemValue)}
-        style={styles.input}
+      <View style={styles.inputPicker}>
+        <Picker
+          selectedValue={donante}
+          onValueChange={(itemValue) => setDonante(itemValue)}
+          style={styles.input}
         >
-        <Picker.Item label="Toca aqui para seleccionar una opción" value="" />
-        <Picker.Item label="Sí" value="Sí" />
-        <Picker.Item label="No" value="No" />
-      </Picker>
-
+          <Picker.Item label="Toca aqui para seleccionar una opción" value="" />
+          <Picker.Item label="Sí" value="Sí" />
+          <Picker.Item label="No" value="No" />
+        </Picker>
+      </View>
       <View style={styles.buttonContainer}>
         <Button
           title="Guardar Datos"
@@ -159,29 +193,36 @@ const InsertEdirDatos = () => {
   );
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'white',
-      padding: 10,
-    },
-    header: {
-      color: 'black',
-      fontSize: 18,
-      marginBottom: 5,
-    },
-    input: {
-      height: 40,
-      borderColor: 'black',
-      borderWidth: 1,
-      marginBottom: 20,
-      color: 'black',
-      paddingLeft: 5,
-    },
-    buttonContainer: {
-      width: '50%',
-      alignSelf: 'flex-end',
-      marginBottom: 30,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 10,
+  },
+  header: {
+    color: 'black',
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  input: {
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginBottom: 20,
+    color: 'black',
+    paddingLeft: 5,
+  },
+  inputPicker: {
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginBottom: 15,
+    justifyContent: 'center', // Asegúrate de que el texto esté centrado verticalmente
+  },
+  buttonContainer: {
+    width: '50%',
+    alignSelf: 'flex-end',
+    marginBottom: 30,
+  },
 });
 export default InsertEdirDatos;
 
